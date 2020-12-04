dc1de0d5590713b998ce64e9dd36358f35f32ab5
jcjveraa <3942301+jcjveraa@users.noreply.github.com>
2020-12-04 17:13:28 +0100

    day1 star 1

--- day_01/Program.cs
+++ day_01/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace day_01
 {
@@ -8,15 +10,42 @@ namespace day_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            List<long> lists = LoadTestInts();
+            bool liveMode = false;
+            foreach (var item in args)
+            {
+                if (item == "-- live")
+                {
+                    liveMode = true;
+                }
+            }
+            List<long> inputInts = LoadInts(liveMode);
 
-            
+            long a = 0, b = 0;
+
+            for (int i = 0; i < inputInts.Count; i++)
+            {
+                for (int j = i + 1; j < inputInts.Count; j++)
+                {
+                    if (inputInts[i] + inputInts[j] == 2020)
+                    {
+                        a = inputInts[i];
+                        b = inputInts[j];
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("a={0}, b={1}, m={2}", a, b, a * b);
         }
 
-        static private List<long> LoadTestInts()
+        static private List<long> LoadInts(bool liveMode)
         {
-            string[] vs = File.ReadAllLines("test_input.txt");
+            string fileName = "test_input.txt";
+            if (liveMode)
+            {
+                fileName = "input.txt";
+            }
+            string[] vs = File.ReadAllLines(fileName);
             List<long> result = new List<long>();
             foreach (string item in vs)
             {
