ab1f55f30eb0163a9b861a3c01f8b70cf6a4b1f2
jcjveraa <3942301+jcjveraa@users.noreply.github.com>
2020-12-04 16:34:45 +0100

    first commit

---
+++ day_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<long> lists = LoadTestInts();
+
+            
+        }
+
+        static private List<long> LoadTestInts()
+        {
+            string[] vs = File.ReadAllLines("test_input.txt");
+            List<long> result = new List<long>();
+            foreach (string item in vs)
+            {
+                result.Add(Int64.Parse(item));
+            }
+
+            return result;
+        }
+    }
+}
