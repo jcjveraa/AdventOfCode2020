2555c977ae21b2b2f25ce5f7e1a3a105844a1649
jcjveraa <3942301+jcjveraa@users.noreply.github.com>
2020-12-06 10:14:39 +0100

    day 6 star 2

--- day_06/Program.cs
+++ day_06/Program.cs
@@ -14,27 +14,59 @@ namespace day_06
             List<string> input = fileLoaders.LoadStrings();
 
             var set = new HashSet<char>();
-            var result = new List<int>();
+            var part1result = new List<int>();
+            var part2buffer = new List<string>();
+            int part2counter = 0;
+
             foreach (string line in input)
             {
-                // Console.WriteLine(line);
                 if (line.Trim().Length == 0)
                 {
-                    result.Add(set.Count);
-                    set = new HashSet<char>();
+                    StoreResults(ref set, part1result, ref part2buffer, ref part2counter);
                 }
                 else
                 {
+                    part2buffer.Add(line);
                     foreach (char c in line)
                     {
                         set.Add(c);
                     }
                 }
             }
-            result.Add(set.Count);
+            StoreResults(ref set, part1result, ref part2buffer, ref part2counter);
 
-            var part1 = result.Aggregate((x, next) => x + next);
+            var part1 = part1result.Aggregate((x, next) => x + next);
             Console.WriteLine("Part 1 result is {0}", part1);
+            Console.WriteLine("Part 2 result is {0}", part2counter);
+        }
+
+        private static void StoreResults(ref HashSet<char> set, List<int> part1result, ref List<string> part2buffer, ref int part2counter)
+        {
+            part1result.Add(set.Count);
+            foreach (char c in set)
+            {
+                if (CharPresentInAllLines(c, part2buffer))
+                {
+                    part2counter++;
+                }
+            }
+
+            set = new HashSet<char>();
+            part2buffer = new List<string>();
+        }
+
+        static bool CharPresentInAllLines(char c, List<string> strL)
+        {
+
+            foreach (var line in strL)
+            {
+                if (!line.Any(x => x == c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
