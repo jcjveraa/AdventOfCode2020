f2e87438a49674feef81f55a15f1fe1e24232bf7
jcjveraa <3942301+jcjveraa@users.noreply.github.com>
2020-12-06 10:01:12 +0100

    day 6 part 1

---
+++ day_06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using _00_common;
+
+namespace day_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileLoaders fileLoaders = new FileLoaders(args);
+            List<string> input = fileLoaders.LoadStrings();
+
+            var set = new HashSet<char>();
+            var result = new List<int>();
+            foreach (string line in input)
+            {
+                // Console.WriteLine(line);
+                if (line.Trim().Length == 0)
+                {
+                    result.Add(set.Count);
+                    set = new HashSet<char>();
+                }
+                else
+                {
+                    foreach (char c in line)
+                    {
+                        set.Add(c);
+                    }
+                }
+            }
+            result.Add(set.Count);
+
+            var part1 = result.Aggregate((x, next) => x + next);
+            Console.WriteLine("Part 1 result is {0}", part1);
+        }
+    }
+}
